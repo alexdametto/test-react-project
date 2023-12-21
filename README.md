189d4e747932937f457406d2453f0400dc8eba9e
Alex Dametto <dametto.alex@gmail.com>
2023-12-21 11:21:09 +0100

    Upload project

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# test-react-project
-Project to test a custom library import
+# Test React Project
+
+> [!WARNING]
+> This repo is a POC to test the creation of a [react component library](https://github.com/alexdametto/test-react-library). This repo is the react project that imports that library.
+
+To run the following app just:
+
+1. Run `yarn install`
+2. Run `yarn start`
+
+The library components are used in `App.jsx` file.
---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { Button, Text } from '@alexdametto/test-react-library';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <div className='bg-yellow-500 p-2'>
+          <Button className='underline' />
+          <Text />
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
